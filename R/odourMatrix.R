84a5271fad77e98f39f6546bcf02b6262a743ceb
Shahar Frechter <sfrechter@gmail.com>
2015-03-06 16:20:54 +0000

    add documentation

--- R/odourMatrix.R
+++ R/odourMatrix.R
@@ -1,6 +1,14 @@
-# given an odour response matrix with columns for different odours
-# and rows for different sweep timepoints, reorder columns in desired
-# order adding NAs for missing odours
+#' Reorder odour response matrix adding nas as necessary
+#'
+#' given an odour response matrix with columns for different odours and rows for
+#' different sweep timepoints, reorder columns in desired order adding NAs for
+#' missing odours
+#'
+#' @param mat odour response matrix where odours are coloumns and cells are rows
+#' @param odours character vector of odour names
+#' @return gives back a tidyied up matrix with coloumns aorderd by
+#' \code{odours} adding \code{NA}s for missing odour response data
+#' @export
 addnacols<-function(mat,odours){
   odours_we_have=colnames(mat)
   missing_odours=setdiff(odours,odours_we_have)
