8f526bf186ac049e4ce61ed6f299d62e52821003
Shahar Frechter <sfrechter@gmail.com>
2015-03-09 16:24:45 +0000

    Add jet.colors
    * was in SpikeClustering.R

---
+++ R/colors.R
@@ -0,0 +1,4 @@
+#' Return a colour palette function
+#' @seealso \code{\link{colorRampPalette}}
+#' @param n Number of colours in palette
+jet.colors<-colorRampPalette(c('navy','cyan','yellow','red'))
