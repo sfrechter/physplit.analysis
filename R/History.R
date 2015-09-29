df00c49cd3f0f791244eb2b02c75f0f934983808
stootoon <sina.tootoonian@gmail.com>
2015-09-29 18:36:37 +0100

    History can now be taken at a specified set of time points.

--- R/History.R
+++ R/History.R
@@ -1,14 +1,21 @@
-HISTORY <- function(history, keepHistory, iter, numIters, ...){
+HISTORY <- function(history, keepHistory, iter, numIters, at=NULL, ...){
     if (keepHistory){
+        if (is.null(at))
+            at = 1:numIters;       
         fields = list(...);
         values = lapply(fields, FUN=function(x) eval.parent(as.symbol(x),n=3));
 
-        for (i in 1:length(fields)){
-            fld = fields[[i]];
-            d = dim(values[[i]]);
-            if (is.null(history[[fld]]))
-                history[[fld]] = drop(array(dim=c(d,numIters)));
-            history[[fld]][prod(d)*(iter-1) + (1:prod(d))] = values[[i]];
+        ihist = which(at==iter);
+        if (length(ihist)>0){
+            for (ih in ihist){
+                for (i in 1:length(fields)){
+                    fld = fields[[i]];
+                    d = dim(values[[i]]);
+                    if (is.null(history[[fld]]))
+                        history[[fld]] = drop(array(dim=c(d,length(at))));
+                    history[[fld]][prod(d)*(ih-1) + (1:prod(d))] = values[[i]];
+                }
+            }
         }
     }
     history;
