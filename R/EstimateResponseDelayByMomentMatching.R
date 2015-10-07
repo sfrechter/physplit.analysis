3ba4a8f193a9b26a8a97583ca3e3288654bddb53
stootoon <sina.tootoonian@gmail.com>
2015-10-07 19:11:50 +0100

    Modified to allow for a minimum delay and allow the option of fitting the odor profile itself (what we were doing before) if learning the parameters doesn't succeed.

--- R/EstimateResponseDelayByMomentMatching.R
+++ R/EstimateResponseDelayByMomentMatching.R
@@ -10,6 +10,9 @@
 #' @param x0 A binary length T vector indicating the odor window.
 #' @param verbose Whether to print out the results
 #' @param plot Whether to plot the delayed exponential on top of the firing rate.
+#' @param minDelay The minimum delay value that can be used.
+#' @param minDelay The minimum delay value that can be used.
+#' @param fitOdorProfileOnError Whether to fit the odor profile itself if learning the filter parameters failed.
 #' @return A list consisting of
 #'
 #'   \item{a0}{The amplitude of the exponential.}
@@ -24,7 +27,7 @@
 #'
 #'   \item{status}{A string. "OK" if the parameters could be learned, "ERROR" if not.}
 #' @export
-EstimateResponseDelayByMomentMatching <- function(y, x0, verbose = FALSE, plot=FALSE){
+EstimateResponseDelayByMomentMatching <- function(y, x0, verbose = FALSE, plot=FALSE, minDelay = 0, fitOdorProfileOnError = TRUE){
     t0 = min(which(x0>0));
     l0 = mean(y[1:(t0-1)]);
     Tp = sum(x0);
@@ -43,11 +46,17 @@ EstimateResponseDelayByMomentMatching <- function(y, x0, verbose = FALSE, plot=F
                    );
     ## If there's too much noise this sometimes doesn't work.
     ## so just assume 0 delay.
+    a1 = NULL;
+    a0 = NULL;
+    tau = NULL;
+    delay = 0;
+
     if (is.null(res)){
         status = "ERROR";
-        a1 = NULL;
-        a0 = NULL;
-        delay = 0;
+        if (fitOdorProfileOnError){
+            cc = ccf(y,x0,plot=FALSE,lag.max=length(y));
+            delay = max(cc$lag[which.max(cc$acf)], minDelay);                     
+        }
     }else{
          status = "OK";
          tau = res$root;
@@ -55,7 +64,7 @@ EstimateResponseDelayByMomentMatching <- function(y, x0, verbose = FALSE, plot=F
          a0 = b*(1 - a1);
          f = stats::filter(x0, a1, method="recursive")*a0;
          cc = ccf(y,f,plot=FALSE,lag.max=length(y));
-         delay = cc$lag[which.max(cc$acf)];
+         delay = max(cc$lag[which.max(cc$acf)], minDelay);
      }
 
     if (verbose){
