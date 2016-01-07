4d9182dbc4f3b5a14258334bc3bc3f1dc74c7b26
Gregory Jefferis <jefferis@gmail.com>
2016-01-07 14:13:03 +0000

    export jet.colors

--- R/colors.R
+++ R/colors.R
@@ -1,4 +1,5 @@
 #' Return a colour palette function
 #' @seealso \code{\link{colorRampPalette}}
 #' @param n Number of colours in palette
+#' @export
 jet.colors<-colorRampPalette(c('navy','cyan','yellow','red'))
